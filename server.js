ed8e2090fa8ea7ecc77cd9977f0c1d6a6d6307e4
Ganga-tharank <154905189+Ganga-tharank@users.noreply.github.com>
2024-07-25 16:36:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3001' }));
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Task Management API');
